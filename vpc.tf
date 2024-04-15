a16239c858b31122084655e90fc9d6a02415993d
GORI ADENUGA <yadenuga@yahoo.com>
2024-04-15 11:11:45 -0400

    commit

--- vpc.tf
+++ vpc.tf
@@ -371,7 +371,7 @@ resource "aws_launch_template" "Dev-LT" {
   instance_initiated_shutdown_behavior = "terminate"
 
   instance_type = "t2.micro"
-  key_name = 
+  key_name = "terraform"
 
   monitoring {
     enabled = true
